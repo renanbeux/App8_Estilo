82e67309f38a586536cd4a78d13c2adbef431397
Renan Beux <nanerxueb@hotmail.com>
2018-08-21 11:54:36 -0300

    21/08/18 11:54

---
+++ App8_Estilo/App8_Estilo/Menu/Master.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8_Estilo.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+    {
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPagina1(object sender, EventArgs args)
+        {
+            //Detail = new NavigationPage(new Pagina);
+            IsPresented = false;
+        }
+
+    }
+}
